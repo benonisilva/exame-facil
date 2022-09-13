60d43ce58ef60af82eceb086e54b28da876054e3
Benoni Marinho da Silva <b.marinho.da.silva@avanade.com>
2022-09-13 20:20:04 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Exame Facil',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
